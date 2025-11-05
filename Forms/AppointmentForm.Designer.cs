26a2925201dba1351fc592ad708788c02563c8d6
Chris Whaley <brto759@gmail.com>
2025-11-05 11:39:05 -0500

    Applied time conversion logic and validations for the Appointment Form

--- Forms/AppointmentForm.Designer.cs
+++ Forms/AppointmentForm.Designer.cs
@@ -28,12 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Appointment";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppointmentForm));
+            cancel_btn = new Button();
+            save_btn = new Button();
+            AppointmentFormTitle = new Label();
+            customerNameLabel = new Label();
+            ConsultantNameLabel = new Label();
+            DescriptionLabel = new Label();
+            LocationLabel = new Label();
+            VisitTypeLabel = new Label();
+            AppointmentDayLabel = new Label();
+            AppointmentTimeLabel = new Label();
+            AppointmentDatePicker = new DateTimePicker();
+            AppointmentTimeComboBox = new ComboBox();
+            LocationComboBox = new ComboBox();
+            VisitTypeComboBox = new ComboBox();
+            DescriptionTextBox = new TextBox();
+            ConsultantComboBox = new ComboBox();
+            CustomerNameComboBox = new ComboBox();
+            errorProvider = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider).BeginInit();
+            SuspendLayout();
+            // 
+            // cancel_btn
+            // 
+            cancel_btn.Location = new Point(124, 570);
+            cancel_btn.Name = "cancel_btn";
+            cancel_btn.Size = new Size(112, 51);
+            cancel_btn.TabIndex = 0;
+            cancel_btn.Text = "Cancel";
+            cancel_btn.UseVisualStyleBackColor = true;
+            cancel_btn.Click += cancel_btn_Click;
+            // 
+            // save_btn
+            // 
+            save_btn.Location = new Point(305, 570);
+            save_btn.Name = "save_btn";
+            save_btn.Size = new Size(112, 51);
+            save_btn.TabIndex = 1;
+            save_btn.Text = "Save";
+            save_btn.UseVisualStyleBackColor = true;
+            save_btn.Click += save_btn_Click;
+            // 
+            // AppointmentFormTitle
+            // 
+            AppointmentFormTitle.AutoSize = true;
+            AppointmentFormTitle.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AppointmentFormTitle.Location = new Point(178, 39);
+            AppointmentFormTitle.Name = "AppointmentFormTitle";
+            AppointmentFormTitle.Size = new Size(196, 30);
+            AppointmentFormTitle.TabIndex = 2;
+            AppointmentFormTitle.Text = "[AppointmentTitle]";
+            // 
+            // customerNameLabel
+            // 
+            customerNameLabel.AutoSize = true;
+            customerNameLabel.Location = new Point(65, 95);
+            customerNameLabel.Name = "customerNameLabel";
+            customerNameLabel.Size = new Size(94, 15);
+            customerNameLabel.TabIndex = 3;
+            customerNameLabel.Text = "Customer Name";
+            // 
+            // ConsultantNameLabel
+            // 
+            ConsultantNameLabel.AutoSize = true;
+            ConsultantNameLabel.Location = new Point(65, 169);
+            ConsultantNameLabel.Name = "ConsultantNameLabel";
+            ConsultantNameLabel.Size = new Size(65, 15);
+            ConsultantNameLabel.TabIndex = 4;
+            ConsultantNameLabel.Text = "Consultant";
+            // 
+            // DescriptionLabel
+            // 
+            DescriptionLabel.AutoSize = true;
+            DescriptionLabel.Location = new Point(65, 255);
+            DescriptionLabel.Name = "DescriptionLabel";
+            DescriptionLabel.Size = new Size(67, 15);
+            DescriptionLabel.TabIndex = 5;
+            DescriptionLabel.Text = "Description";
+            // 
+            // LocationLabel
+            // 
+            LocationLabel.AutoSize = true;
+            LocationLabel.Location = new Point(124, 330);
+            LocationLabel.Name = "LocationLabel";
+            LocationLabel.Size = new Size(53, 15);
+            LocationLabel.TabIndex = 6;
+            LocationLabel.Text = "Location";
+            // 
+            // VisitTypeLabel
+            // 
+            VisitTypeLabel.AutoSize = true;
+            VisitTypeLabel.Location = new Point(305, 330);
+            VisitTypeLabel.Name = "VisitTypeLabel";
+            VisitTypeLabel.Size = new Size(57, 15);
+            VisitTypeLabel.TabIndex = 7;
+            VisitTypeLabel.Text = "Visit Type";
+            // 
+            // AppointmentDayLabel
+            // 
+            AppointmentDayLabel.AutoSize = true;
+            AppointmentDayLabel.Location = new Point(124, 396);
+            AppointmentDayLabel.Name = "AppointmentDayLabel";
+            AppointmentDayLabel.Size = new Size(101, 15);
+            AppointmentDayLabel.TabIndex = 8;
+            AppointmentDayLabel.Text = "Appointment Day";
+            // 
+            // AppointmentTimeLabel
+            // 
+            AppointmentTimeLabel.AutoSize = true;
+            AppointmentTimeLabel.Location = new Point(124, 463);
+            AppointmentTimeLabel.Name = "AppointmentTimeLabel";
+            AppointmentTimeLabel.Size = new Size(108, 15);
+            AppointmentTimeLabel.TabIndex = 9;
+            AppointmentTimeLabel.Text = "Appointment Time";
+            // 
+            // AppointmentDatePicker
+            // 
+            AppointmentDatePicker.Location = new Point(124, 414);
+            AppointmentDatePicker.MinDate = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            AppointmentDatePicker.Name = "AppointmentDatePicker";
+            AppointmentDatePicker.Size = new Size(316, 23);
+            AppointmentDatePicker.TabIndex = 10;
+            AppointmentDatePicker.ValueChanged += AppointmentDatePicker_ValueChanged;
+            // 
+            // AppointmentTimeComboBox
+            // 
+            AppointmentTimeComboBox.FormattingEnabled = true;
+            AppointmentTimeComboBox.Location = new Point(124, 481);
+            AppointmentTimeComboBox.Name = "AppointmentTimeComboBox";
+            AppointmentTimeComboBox.Size = new Size(153, 23);
+            AppointmentTimeComboBox.TabIndex = 11;
+            AppointmentTimeComboBox.SelectedIndexChanged += AppointmentTimeComboBox_SelectedIndexChanged;
+            // 
+            // LocationComboBox
+            // 
+            LocationComboBox.FormattingEnabled = true;
+            LocationComboBox.Location = new Point(124, 357);
+            LocationComboBox.Name = "LocationComboBox";
+            LocationComboBox.Size = new Size(136, 23);
+            LocationComboBox.TabIndex = 12;
+            LocationComboBox.SelectedIndexChanged += LocationComboBox_SelectedIndexChanged;
+            // 
+            // VisitTypeComboBox
+            // 
+            VisitTypeComboBox.FormattingEnabled = true;
+            VisitTypeComboBox.Location = new Point(305, 357);
+            VisitTypeComboBox.Name = "VisitTypeComboBox";
+            VisitTypeComboBox.Size = new Size(135, 23);
+            VisitTypeComboBox.TabIndex = 13;
+            VisitTypeComboBox.SelectedIndexChanged += VisitTypeComboBox_SelectedIndexChanged;
+            // 
+            // DescriptionTextBox
+            // 
+            DescriptionTextBox.Location = new Point(65, 284);
+            DescriptionTextBox.Name = "DescriptionTextBox";
+            DescriptionTextBox.Size = new Size(375, 23);
+            DescriptionTextBox.TabIndex = 14;
+            DescriptionTextBox.TextChanged += DescriptionTextBox_TextChanged;
+            // 
+            // ConsultantComboBox
+            // 
+            ConsultantComboBox.FormattingEnabled = true;
+            ConsultantComboBox.Location = new Point(65, 197);
+            ConsultantComboBox.Name = "ConsultantComboBox";
+            ConsultantComboBox.Size = new Size(375, 23);
+            ConsultantComboBox.TabIndex = 15;
+            ConsultantComboBox.SelectedIndexChanged += ConsultantComboBox_SelectedIndexChanged;
+            // 
+            // CustomerNameComboBox
+            // 
+            CustomerNameComboBox.FormattingEnabled = true;
+            CustomerNameComboBox.Location = new Point(65, 124);
+            CustomerNameComboBox.Name = "CustomerNameComboBox";
+            CustomerNameComboBox.Size = new Size(375, 23);
+            CustomerNameComboBox.TabIndex = 16;
+            CustomerNameComboBox.SelectedIndexChanged += CustomerNameComboBox_SelectedIndexChanged;
+            // 
+            // errorProvider
+            // 
+            errorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            errorProvider.ContainerControl = this;
+            errorProvider.Icon = (Icon)resources.GetObject("errorProvider.Icon");
+            // 
+            // AppointmentForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(558, 647);
+            Controls.Add(CustomerNameComboBox);
+            Controls.Add(ConsultantComboBox);
+            Controls.Add(DescriptionTextBox);
+            Controls.Add(VisitTypeComboBox);
+            Controls.Add(LocationComboBox);
+            Controls.Add(AppointmentTimeComboBox);
+            Controls.Add(AppointmentDatePicker);
+            Controls.Add(AppointmentTimeLabel);
+            Controls.Add(AppointmentDayLabel);
+            Controls.Add(VisitTypeLabel);
+            Controls.Add(LocationLabel);
+            Controls.Add(DescriptionLabel);
+            Controls.Add(ConsultantNameLabel);
+            Controls.Add(customerNameLabel);
+            Controls.Add(AppointmentFormTitle);
+            Controls.Add(save_btn);
+            Controls.Add(cancel_btn);
+            Name = "AppointmentForm";
+            Text = "Appointment";
+            ((System.ComponentModel.ISupportInitialize)errorProvider).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button cancel_btn;
+        private Button save_btn;
+        private Label AppointmentFormTitle;
+        private Label customerNameLabel;
+        private Label ConsultantNameLabel;
+        private Label DescriptionLabel;
+        private Label LocationLabel;
+        private Label VisitTypeLabel;
+        private Label AppointmentDayLabel;
+        private Label AppointmentTimeLabel;
+        private DateTimePicker AppointmentDatePicker;
+        private ComboBox AppointmentTimeComboBox;
+        private ComboBox LocationComboBox;
+        private ComboBox VisitTypeComboBox;
+        private TextBox DescriptionTextBox;
+        private ComboBox ConsultantComboBox;
+        private ComboBox CustomerNameComboBox;
+        private ErrorProvider errorProvider;
     }
 }
--- Forms/AppointmentForm.cs
+++ Forms/AppointmentForm.cs
@@ -1,4 +1,6 @@
 ﻿using Appointment_Manager.Controller;
+using Appointment_Manager.Controller.Utils;
+using Appointment_Manager.Model.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,19 +15,284 @@ namespace Appointment_Manager.Forms
 {
     public partial class AppointmentForm : Form
     {
-        public Appointment MainFormInstance { get; set; }
-
         private AppointmentController appointmentController = new AppointmentController();
 
         private CustomerController customerController = new CustomerController();
 
         private UserController userController = new UserController();
 
-        private string appointmentId;
+        private string appointmentId = string.Empty;
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public bool IsUpdate { get; set; }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Action<bool> RefreshHandler { get; set; }
+
+        private struct TimeSlot
+        {
+            public string DisplayText { get; set; }
+            public TimeSpan Value { get; set; }
+        }
         public AppointmentForm()
         {
             InitializeComponent();
-            LoadForm();
+            LoadFormControls();
         }
+
+        #region EventHandlers
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            //Field Validation
+            if (!FieldsAreValid())
+            {
+                MessageBox.Show("Please select a value for all fields marked with an error.", "Invalid input.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Time Conversion
+            DateTime datePart = AppointmentDatePicker.Value.Date;
+            TimeSpan timePart = (TimeSlot)AppointmentTimeComboBox.SelectedValue;
+
+            DateTime startTimeLocal = datePart + timePart;
+            DateTime endTimeLocal = startTimeLocal.AddMinutes(30); // Makes appointments 30 minutes long
+
+            DateTime startTimeEST = TimeUtil.ConvertToEST(startTimeLocal);
+            DateTime endTimeEST = TimeUtil.ConvertToEST(endTimeLocal);
+
+            if (appointmentController.CheckForOverlappingAppointments(startTimeEST, endTimeEST, appointmentId))
+            {
+                MessageBox.Show("This appointment overlaps with an existing appointment for this consultant. Please choose a different time.", "Overlap Detected.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Create data payload for AppointmentController
+            var appointmentData = new Dictionary<string, object>
+            {
+                { "AppointmentId", this.appointmentId },
+                { "CustomerId", CustomerNameComboBox.SelectedValue }, // Assumes ValueMember is CustomerId
+                { "UserId", ConsultantComboBox.SelectedValue },     // Assumes ValueMember is UserId
+                { "Description", DescriptionTextBox.Text.Trim() },
+                { "Location", LocationComboBox.SelectedItem.ToString() },
+                { "Type", VisitTypeComboBox.SelectedItem.ToString() },
+                { "Start", startTimeEST }, // Pass the converted EST time
+                { "End", endTimeEST }      // Pass the converted EST time
+            };
+
+            bool success = appointmentController.SaveAppointment(appointmentData, IsUpdate);
+
+            if (success)
+            {
+                RefreshHandler?.Invoke(IsUpdate);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Failed to save appointment.", "Save Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cancel_btn_Click(object sender, EventArgs e)
+        {
+            RefreshHandler?.Invoke(false);
+            this.Close();
+        }
+
+        private void AppointmentTimeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear error provider
+            if (sender is ComboBox comboBox && comboBox.SelectedIndex != -1)
+            {
+                errorProvider.SetError(comboBox, "");
+            }
+        }
+
+        private void AppointmentDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LocationComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear error provider
+            if (sender is ComboBox comboBox && comboBox.SelectedIndex != -1)
+            {
+                errorProvider.SetError(comboBox, "");
+            }
+        }
+
+        private void VisitTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear error provider
+            if (sender is ComboBox comboBox && comboBox.SelectedIndex != -1)
+            {
+                errorProvider.SetError(comboBox, "");
+            }
+        }
+
+        private void DescriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(DescriptionTextBox.Text))
+            {
+                errorProvider.SetError(DescriptionTextBox, "");
+            }
+        }
+
+        private void ConsultantComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear error provider
+            if (sender is ComboBox comboBox && comboBox.SelectedIndex != -1)
+            {
+                errorProvider.SetError(comboBox, "");
+            }
+        }
+
+        private void CustomerNameComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear error provider
+            if (sender is ComboBox comboBox && comboBox.SelectedIndex != -1)
+            {
+                errorProvider.SetError(comboBox, "");
+            }
+        }
+        #endregion
+
+        #region HelperMethods
+
+        private void LoadFormControls()
+        {
+            AppointmentDatePicker.MinDate = DateTime.Today;
+
+            PopulateTimeComboBox();
+
+
+            //Set default (unselected) state
+            CustomerNameComboBox.SelectedIndex = -1;
+            ConsultantComboBox.SelectedIndex = -1;
+            LocationComboBox.SelectedIndex = -1;
+            VisitTypeComboBox.SelectedIndex = -1;
+
+            // Wire up validation event handlers
+            CustomerNameComboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+            ConsultantComboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+            LocationComboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+            VisitTypeComboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+            AppointmentTimeComboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+
+        }
+
+        /// <summary>
+        /// Populates the time ComboBox with 30-min intervals during business hours (9am-5pm).
+        /// </summary>
+        private void PopulateTimeComboBox()
+        {
+            var timeSlots = new List<TimeSlot>();
+            TimeSpan startTime = new TimeSpan(9, 0, 0);  // 9:00 AM
+            TimeSpan endTime = new TimeSpan(17, 0, 0); // 5:00 PM
+
+            while (startTime < endTime)
+            {
+                TimeSpan slotEnd = startTime.Add(new TimeSpan(0, 30, 0));
+                timeSlots.Add(new TimeSlot
+                {
+                    // Format: "09:00 - 09:30"
+                    Display = $"{startTime:hh\\:mm} - {slotEnd:hh\\:mm}",
+                    Value = startTime
+                });
+                startTime = slotEnd;
+            }
+
+            AppointmentTimeComboBox.DataSource = timeSlots;
+            AppointmentTimeComboBox.DisplayMember = "Display";
+            AppointmentTimeComboBox.ValueMember = "Value";
+            AppointmentTimeComboBox.SelectedIndex = -1;
+        }
+
+        private void UpdateAppointmentTitle(bool isUpdate)
+        {
+            this.IsUpdate = isUpdate;
+            AppointmentFormTitle.Text = isUpdate ? "Update Appointment" : "Add Appointment";
+        }
+
+        private void PopulateAppointmentFields(DataGridViewRow row)
+        {
+            // Get LOCAL time
+            DateTime startTimeLocal = (DateTime)row.Cells["Start"].Value;
+
+            // Set simple fields
+            DescriptionTextBox.Text = row.Cells["Description"].Value.ToString();
+            LocationComboBox.SelectedItem = row.Cells["Location"].Value.ToString();
+            VisitTypeComboBox.SelectedItem = row.Cells["Visit Type"].Value.ToString();
+            CustomerNameComboBox.SelectedValue = (int)row.Cells["CustomerId"].Value;
+            ConsultantComboBox.SelectedValue = (int)row.Cells["UserId"].Value;
+
+            // --- SPLIT DATETIME FOR UI CONTROLS ---
+            AppointmentDatePicker.Value = startTimeLocal.Date;
+
+            // Find the matching TimeSpan in the ComboBox items
+            // This is safer than using SelectedItem directly
+            TimeSpan timeValue = startTimeLocal.TimeOfDay;
+            foreach (TimeSlot slot in AppointmentTimeComboBox.Items)
+            {
+                if (slot.Value == timeValue)
+                {
+                    AppointmentTimeComboBox.SelectedItem = slot;
+                    break;
+                }
+            }
+
+            appointmentId = row.Cells["appointmentId"].Value.ToString();
+
+        }
+
+        /// <summary>
+        /// Validates that all required controls have a selection.
+        /// </summary>
+        private bool FieldsAreValid()
+        {
+            bool valid = true;
+
+            // Validate ComboBoxes
+            if (CustomerNameComboBox.SelectedIndex == -1)
+            {
+                errorProvider.SetError(CustomerNameComboBox, "Please select a customer.");
+                valid = false;
+            }
+            if (ConsultantComboBox.SelectedIndex == -1)
+            {
+                errorProvider.SetError(ConsultantComboBox, "Please select a consultant.");
+                valid = false;
+            }
+            if (LocationComboBox.SelectedIndex == -1)
+            {
+                errorProvider.SetError(LocationComboBox, "Please select a location.");
+                valid = false;
+            }
+            if (VisitTypeComboBox.SelectedIndex == -1)
+            {
+                errorProvider.SetError(VisitTypeComboBox, "Please select a visit type.");
+                valid = false;
+            }
+            if (AppointmentTimeComboBox.SelectedIndex == -1)
+            {
+                errorProvider.SetError(AppointmentTimeComboBox, "Please select a time.");
+                valid = false;
+            }
+
+            // Validate TextBox
+            if (string.IsNullOrWhiteSpace(DescriptionTextBox.Text))
+            {
+                errorProvider.SetError(DescriptionTextBox, "Description cannot be empty.");
+                valid = false;
+            }
+
+            return valid;
+        }
+        #endregion
+
+
     }
 }
